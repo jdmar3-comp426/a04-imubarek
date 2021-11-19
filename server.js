7b4ead310d5b667bacdb5686ea069d00e5305abc
Ibrahim Mubarek <42622248+imubarek@users.noreply.github.com>
2021-11-19 14:04:12 -0500

    Hashed the inputed password for create

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.get("/app/", (req, res, next) => {
 //new/user or new/
 app.post("/app/new/", (req, res) => {	
 	const stmt = db.prepare("INSERT INTO userinfo (user,pass) VALUES (?,?)") ; 
-	const info = stmt.run(req.body.user, req.body.pass); 
+	const info = stmt.run(req.body.user, md5(req.body.pass)); 
 	res.status(201).json({"message" : "1 record created: ID " + info.lastInsertRowid + " (201)"}) ; 
 });
 
